b2cb1a640ed20b60e6a0ecd0e036610b43a72d31
Thomas Bethell <tbethell09@gmail.com>
2015-03-02 03:29:23 +0000

    fixed :url bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
     def new
     end
     
+     def index
+        @articles = Article.all
+     end
+    
     def show
         @article = Article.find(params[:id])
     end
